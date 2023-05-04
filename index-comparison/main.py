f86489143e37c183ddb4c135c43775196e05cc18
Soenhay <21283059+Soenhay@users.noreply.github.com>
2023-05-04 12:26:25 -0500

    Create main.py

---
+++ index-comparison/main.py
@@ -0,0 +1,11 @@
+import pyfiglet
+import numpy as np, numpy.random
+import matplotlib.pyplot as plt
+
+
+def main():
+    intro = pyfiglet.figlet_format("Index Comparison")
+    print(intro)
+    
+    
+main()
