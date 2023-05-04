3e9662a5626d52642c26c4317aa5ded1533b926a
Josh Boyd <joshua.boyd@tamucc.edu>
2023-05-04 13:02:28 -0500

    Added data, launch settings

--- index-comparison/main.py
+++ index-comparison/main.py
@@ -1,11 +1,18 @@
 import pyfiglet
 import numpy as np, numpy.random
 import matplotlib.pyplot as plt
+import pandas as pd
+from rtreelib import RTree, Rect
+
 
 
 def main():
     intro = pyfiglet.figlet_format("Index Comparison")
     print(intro)
+
+    df1 = pd.read_csv('/input/flightAdsb.csv')
+    df2 = pd.read_csv('/input/flightTelem.csv')
+    df3 = pd.read_csv('/input/flightTelem2.csv')
     
     
 main()
