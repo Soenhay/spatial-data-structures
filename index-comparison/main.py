696235994bbae1e291e805a6bb4db66cbdff778c
Josh Boyd <joshua.boyd@tamucc.edu>
2023-05-05 15:52:26 -0500

    add all data files to one dataframe. output row and column count.

--- index-comparison/main.py
+++ index-comparison/main.py
@@ -10,9 +10,18 @@ def main():
     intro = pyfiglet.figlet_format("Index Comparison")
     print(intro)
 
-    df1 = pd.read_csv('/input/flightAdsb.csv')
-    df2 = pd.read_csv('/input/flightTelem.csv')
-    df3 = pd.read_csv('/input/flightTelem2.csv')
+    dfs = []
+    #Relative paths are relative to current working directory. Since this project is in a sub folder the path needs to include it.
+    dfs.append(pd.read_csv('./index-comparison/input/flightAdsb.csv', usecols = ['flightId','launchDateTimeUTC','landDateTimeUTC','timeStampUtc','lat','lon','alt']))
+    dfs.append(pd.read_csv('./index-comparison/input/flightTelem.csv', usecols = ['flightId','launchDateTimeUTC','landDateTimeUTC','timeStampUtc','lat','lon','alt']))
+    dfs.append(pd.read_csv('./index-comparison/input/flightTelem2.csv', usecols = ['flightId','launchDateTimeUTC','landDateTimeUTC','timeStampUtc','lat','lon','alt']))
+    df = pd.concat(dfs, ignore_index=True)
+
     
+    #print(f'Row count: {len(df.index)}')
+    print(f'Row count: {df.shape[0]}')
+    #print(f'Row count: {ddf[df.columns[0]].count()}')
+    print(f'Column count: {len(df.columns)}')
+    print(f'Memory usage: \n{df.memory_usage()}')
     
 main()
